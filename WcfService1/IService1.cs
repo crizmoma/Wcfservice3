93a40b72f4c197e70f12a1e49a11066d85b04b9c
crizmoma <criztalmontelongo1998@gmail.com>
2020-11-12 20:55:39 -0600

    primer commit para cargar los archivos a GitHub

---
+++ WcfService1/IService1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetIdBiblioteca(int value);
+
+        [OperationContract]
+        Libro GetLibro(Libro libro);
+
+        [OperationContract]
+        string GetIdProducto(int value);
+
+        [OperationContract]
+        Producto GetProducto(Producto producto);
+
+
+        [OperationContract]
+        Alumnos CrearAlumnos();
+
+        [OperationContract]
+        double convertirDolares(double pesos);
+
+        [OperationContract]
+        Alumnos GetDatosAlumnos(Alumnos alumnos);
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+    [DataContract]
+    public class Alumnos
+    {
+        string nombre;
+        int nocontrol;
+        int semestre;
+        float promedio;
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public int NoControl
+        {
+            get { return nocontrol; }
+            set { nocontrol = value; }
+        }
+        [DataMember]
+        public int Semestre
+        {
+            get { return semestre; }
+            set { semestre = value; }
+        }
+        [DataMember]
+        public float Promedio
+        {
+            get { return promedio; }
+            set { promedio = value; }
+        }
+    }
+
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+    [DataContract]
+    public class Producto
+    {
+        string nombre;
+        int id;
+
+        [DataMember]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+    }
+    [DataContract]
+    public class Libro
+    {
+        string titulo;
+        int año;
+        string autor;
+        int id;
+
+        [DataMember]
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+        [DataMember]
+        public int Año
+        {
+            get { return año; }
+            set { año = value; }
+
+        }
+        [DataMember]
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+        //ejemplo1.1
+        public double convertirDolares(double pesos)
+        {
+            return pesos * 15.89;
+        }
+        //metodo 3
+        Libro libro = new Libro();
+        public string GetIdBiblioteca(int value)
+        {
+            if (value == 1)
+            {
+                libro.Titulo = "Calculo Integral";
+                return string.Format("El libro es:" + libro.Titulo);
+            }
+            else if (value == 2)
+            {
+                libro.Titulo = "Metodologia e la informacion";
+                return string.Format("El libro es:" + libro.Titulo);
+            }
+            else if (value == 3)
+            {
+                libro.Titulo = "Algebra lineal";
+                return string.Format("El libro es:" + libro.Titulo);
+            }
+            else if (value == 4)
+            {
+                libro.Titulo = "Programacion";
+                return string.Format("El libro es:" + libro.Titulo);
+            }
+
+            return null;
+
+        }
+        //metodo 2
+        Producto producto = new Producto();
+        public string GetIdProducto(int value)
+        {
+            if (value == 12)
+            {
+                producto.Nombre = "Gansito";
+                return string.Format("su producto es" + producto.Nombre);
+            }
+            else if (value == 06)
+            {
+                producto.Nombre = "Cocacola";
+                return string.Format("su producto es" + producto.Nombre);
+            }
+
+            else if (value == 12)
+            {
+                producto.Nombre = "Pastel";
+                return string.Format("su producto es" + producto.Nombre);
+
+            }
+            return null;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        public Libro GetLibro(Libro libro)
+        {
+            return libro;
+        }
+        public Producto GetProducto(Producto producto)
+        {
+            return producto;
+        }
+
+        public Alumnos GetDatosAlumnos(Alumnos alumnos)
+        {
+            return alumnos;
+        }
+
+        public Alumnos CrearAlumnos()
+        {
+
+            Alumnos alumno = new Alumnos();
+            alumno.Nombre = "Cristal Montelongo";
+            alumno.NoControl = 17030096;
+            alumno.Semestre = 7;
+            alumno.Promedio = 80;
+
+            return alumno;
+
+        }
+    }
+
+}
